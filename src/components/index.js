ad7617df2e05ea1f621400ab29ea64a1ab6977c4
Alexander Baranov <baranov.rc@gmail.com>
2022-05-03 01:24:51 +0300

    add sort component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 import Button from "./button/Button";
 import Header from "../Header";
 import Category from "./category/Category";
-import Footer from '../Footer'
+import Footer from '../Footer';
+import Sort from "./sort/Sort";
 
-export { Button, Header, Category, Footer };
+export { Button, Header, Category, Footer, Sort };
