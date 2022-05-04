6b53f8b653a5ca2d45c08665e0e98f6a344fbd4e
Alexander Baranov <baranov.rc@gmail.com>
2022-05-04 23:01:31 +0300

    add react-route-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Error404 from './404';
+
 import './scss/app.scss'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<App />} />
+        <Route path='*' element={<Error404 />} />
+
+
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
